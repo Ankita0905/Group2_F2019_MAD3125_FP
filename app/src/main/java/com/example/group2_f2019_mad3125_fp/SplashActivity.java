ee2c2d784798724d956104b964c173dab544e167
Ankita Jain <ankita.jain12342@gmail.com>
2019-12-05 14:59:20 -0500

    Splash screen done

--- app/src/main/java/com/example/group2_f2019_mad3125_fp/SplashActivity.java
+++ app/src/main/java/com/example/group2_f2019_mad3125_fp/SplashActivity.java
@@ -2,6 +2,7 @@ package com.example.group2_f2019_mad3125_fp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 public class SplashActivity extends AppCompatActivity {
@@ -9,6 +10,9 @@ public class SplashActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_splash);
+        Intent intent=new Intent(SplashActivity.this,MainActivity.class);
+        startActivity(intent);
+        finish();
+
     }
 }
