f8b66bcb2d25f4c4a79c4e59634d366b85f9b990
Ankita Jain <ankita.jain12342@gmail.com>
2019-12-11 18:26:47 -0500

    updations

--- app/src/main/java/com/example/group2_f2019_mad3125_fp/ui/addemployee/AddEmployeeFragment.java
+++ app/src/main/java/com/example/group2_f2019_mad3125_fp/ui/addemployee/AddEmployeeFragment.java
@@ -191,7 +191,6 @@ public class AddEmployeeFragment extends Fragment {
 
                 addData();
                 getActivity().onBackPressed();
-
                 
 //                fragmentTransaction.replace(R.id.nav_showEmployee, fr);
 //                fragmentTransaction.commit();
