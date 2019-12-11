bc149cc0f2f14f061add9e0903a517aa8ac7d5e5
Ankita Jain <ankita.jain12342@gmail.com>
2019-12-11 18:03:53 -0500

    updations

--- app/src/main/java/com/example/group2_f2019_mad3125_fp/model/employee/Employee.java
+++ app/src/main/java/com/example/group2_f2019_mad3125_fp/model/employee/Employee.java
@@ -35,7 +35,7 @@ public class Employee implements Serializable
       this.name=eName;
       this.employee=eType;
     }
-    
+
     public Employee(String name, int age,String Etype)
     {
         this.name = name;
@@ -53,7 +53,6 @@ public class Employee implements Serializable
 
     public int getAge()
     {
-
         int curDate= Calendar.getInstance().get(Calendar.YEAR);;
         age=curDate-age;
         return age;
