4fb93e078efe06bb790476720f2d1703067e249c
Ankita Jain <ankita.jain12342@gmail.com>
2019-12-11 18:02:03 -0500

    updations

--- app/src/main/java/com/example/group2_f2019_mad3125_fp/model/employee/Employee.java
+++ app/src/main/java/com/example/group2_f2019_mad3125_fp/model/employee/Employee.java
@@ -39,7 +39,8 @@ public class Employee implements Serializable
     }
 
 
-    public Employee(String name, int age,String Etype) {
+    public Employee(String name, int age,String Etype)
+    {
         this.name = name;
         this.age = age;
         this.employee=Etype;
