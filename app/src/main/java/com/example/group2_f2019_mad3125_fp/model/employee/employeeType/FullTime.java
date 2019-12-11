bbffa475222138e70a00560f7a8d2b9ea0d2cefa
Ankita Jain <ankita.jain12342@gmail.com>
2019-12-11 17:53:37 -0500

    updations

--- app/src/main/java/com/example/group2_f2019_mad3125_fp/model/employee/employeeType/FullTime.java
+++ app/src/main/java/com/example/group2_f2019_mad3125_fp/model/employee/employeeType/FullTime.java
@@ -5,7 +5,8 @@ import com.example.group2_f2019_mad3125_fp.model.employee.Employee;
 
 import java.io.Serializable;
 
-public class FullTime extends Employee implements Serializable {
+public class FullTime extends Employee implements Serializable
+{
 
     private double salary;
     private double bonus;
